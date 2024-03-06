837aa6e97a8ebb701a52c4b63f27a623d48c6692
frida <frida@DESKTOP-L72T31C>
2024-03-06 10:24:56 +0100

    primo commit multiserver

---
+++ src/main/java/com/mycompany/chatserverclient/MainChatClient.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chatserverclient;
+
+/**
+ *
+ * @author frida
+ */
+public class MainChatClient {
+   public static void main(String args[])
+	{
+		ChatClient chat = new ChatClient();
+	}  
+}
---
+++ src/main/java/com/mycompany/chatserverclient/MainChatServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chatserverclient;
+
+/**
+ *
+ * @author frida
+ */
+public class MainChatServer {
+    public static void main(String args[])
+	{
+		ChatServer chat = new ChatServer();
+	}
+}
